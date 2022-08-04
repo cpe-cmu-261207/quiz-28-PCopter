14ac85cb72e8a1e5e99ab4cde03725a2e03ddbc0
PCopter <90751258+PCopter@users.noreply.github.com>
2022-08-04 14:14:15 +0700

    TT

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 ให้นักศึกษาสร้างเว็บไซต์สุ่มโปรไฟล์ของผู้ใช้โดยใช้ HTML, CSS และ JavaScript ซึ่งมีรายละเอียดดังในไฟล์ pdf นี้
 
 https://o365cmu-my.sharepoint.com/:b:/g/personal/chayanin_s_cmu_ac_th1/ER4w5BKGHARKrJuDoHgyF08B5J4Ff7gr2xzoAWAIaCeOUg?e=oWDTSr
+https://github.com/cpe-cmu-261207/quiz-28-PCopter.git
+สร้างเพจไม่ได้
 
-## GitHub Pages URL : 
+## GitHub Pages URL :
--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const btnGenerate = document.getElementById("btn-random");
+const gendericon = document.getElementById("span-gender-icon");
+const usercard = document.getElementById("div-user-card");
+const names = document.getElementById("p-name");
+const address = document.getElementById("p-address");
+const email = document.getElementById("p-email");
+const loading = document.getElementById("div-loading-card");
+
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
 }
+
+btnGenerate.onclick = async () => {
+  callApi();
+  usercard = resp.data.result.picture.large;
+  names.innerText = resp.data.result.name;
+  address.innerText = resp.data.result.location;
+  email.innerText = resp.data.result.email;
+};
